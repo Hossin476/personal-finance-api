788f6c2d4b5264f9117ed48e0d15cfec40e696ff
Lahoucine Shail <74148709+Hossin476@users.noreply.github.com>
2023-04-14 22:56:58 +0000

    adding new functionalities to the api

--- finance.py
+++ finance.py
@@ -1,12 +1,13 @@
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
 from marshmallow import Schema, fields, ValidationError
+from bson.objectid import ObjectId
 
 # Initializing Flask app
 app = Flask(__name__)
 
 # database connection
-client = MongoClient('mongodb://127.0.0.1:27017')
+client = MongoClient('mongodb://127.0.0.1:27017/')
 db = client['finance']
 
 # Defining the schema for transactions and budgets
@@ -30,14 +31,46 @@ def add_transaction():
     except ValidationError as err:
         return jsonify(err.messages), 400
     
-    db.transactions.insert_one(transaction)
-    return jsonify({'message': 'Transaction added successfully.'}), 201
+    result = db.transactions.insert_one(transaction)
+    return jsonify({'message': 'Transaction added successfully.', 'id': str(result.inserted_id)}), 201
 
 
 @app.route('/transactions', methods=['GET'])
 def get_transactions():
     transactions = list(db.transactions.find({}, {'_id': 0}))
     return jsonify(transactions)
+
+
+@app.route('/transactions/<transaction_id>', methods=['GET'])
+def get_transaction_by_id(transaction_id):
+    transaction = db.transactions.find_one({'_id': ObjectId(transaction_id)}, {'_id': 0})
+    if transaction:
+        return jsonify(transaction), 200
+    else:
+        return jsonify({'message': 'Transaction not found.'}), 404
+
+
+@app.route('/transactions/<category>', methods=['PUT'])
+def update_transaction(category):
+    try:
+        query = {'category': category}
+        new_transaction = transaction_schema.load(request.json)
+        result = db.transactions.update_one(query, {"$set": new_transaction})
+        if result.modified_count == 1:
+            return jsonify({'message': 'Transaction updated successfully.'}), 200
+        else:
+            return jsonify({'message': 'Transaction not found.'}), 404
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+
+
+@app.route('/transactions/<category>', methods=['DELETE'])
+def delete_transaction(category):
+    result = db.transactions.delete_one({'category': category})
+    if result.deleted_count == 1:
+        return jsonify({'message': 'Transaction deleted successfully.'}), 200
+    else:
+        return jsonify({'message': 'Transaction not found.'}), 404
 
 
 @app.route('/budgets', methods=['POST'])
@@ -47,14 +80,46 @@ def add_budget():
     except ValidationError as err:
         return jsonify(err.messages), 400
     
-    db.budgets.insert_one(budget)
-    return jsonify({'message': 'Budget added successfully.'}), 201
+    result = db.budgets.insert_one(budget)
+    return jsonify({'message': 'Budget added successfully.', 'id': str(result.inserted_id)}), 201
 
 
 @app.route('/budgets', methods=['GET'])
 def get_budgets():
     budgets = list(db.budgets.find({}, {'_id': 0}))
     return jsonify(budgets)
+
+
+@app.route('/budgets/<budget_id>', methods=['GET'])
+def get_budget_by_id(budget_id):
+    budget = db.budgets.find_one({'_id': ObjectId(budget_id)}, {'_id': 0})
+    if budget:
+        return jsonify(budget), 200
+    else:
+        return jsonify({'message': 'Budget not found.'}), 404
+
+
+@app.route('/budgets/<category>', methods=['PUT'])
+def update_budget(category):
+    try:
+        query = {'category': category}
+        new_budget = budget_schema.load(request.json)
+        result = db.budgets.update_one(query, {"$set": new_budget})
+        if result.modified_count == 1:
+            return jsonify({'message': 'Budget updated successfully.'}), 200
+        else:
+            return jsonify({'message': 'Budget not found.'}), 404
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+
+
+@app.route('/budgets/<category>', methods=['DELETE'])
+def delete_budget(category):
+    result = db.budgets.delete_one({'category': category})
+    if result.deleted_count == 1:
+        return jsonify({'message': 'Budget deleted successfully.'}), 200
+    else:
+        return jsonify({'message': 'Budget not found.'}), 404
 
 # Run the app
 if __name__ == '__main__':
